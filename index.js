01295e7a4b3ff2a61766a5e93b7adeb4314caed4
Megan <smith.megan.design@gmail.com>
2021-08-31 15:19:52 -0600

    ready for rollbar integration

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var rollbar = new Rollbar({
 });
 
 app.get('/', (req, res)=>{
-  res.sendFile(path.join(__dirname, 'index.html'))
+  res.sendFile(path.join(__dirname, 'public/index.html'))
   rollbar.info('html file served successfully.')
   })
 
