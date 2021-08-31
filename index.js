c60f89e66c9b2283986242f462e1242289635c6e
Megan <smith.megan.design@gmail.com>
2021-08-31 15:04:54 -0600

    ready for rollbar integration

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const path= require('path')
-// const Rollbar = require('rollbar')
+const Rollbar = require('rollbar')
 
 const app = express()
 
 app.use(express.json())
 
-// var rollbar = new Rollbar({
-//   accessToken: '48919cbac623421f8c4d079e8e09fe2e',
-//   captureUncaught: true,
-//   captureUnhandledRejections: true
-// });
+var rollbar = new Rollbar({
+  accessToken: '48919cbac623421f8c4d079e8e09fe2e',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
 
 app.get('/', (req, res)=>{
   res.sendFile(path.join(__dirname, 'index.html'))
-  // rollbar.info('html file served successfully.')
+  rollbar.info('html file served successfully.')
   })
 
 const port=process.env.PORT || 5545
