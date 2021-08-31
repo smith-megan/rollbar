b42a632ed5d14c93053542eb1e21af0aa5b76322
Megan <smith.megan.design@gmail.com>
2021-08-31 16:04:37 -0600

    reset

--- index.js
+++ index.js
@@ -5,39 +5,47 @@ const Rollbar = require('rollbar')
 const app = express()
 
 app.use(express.json())
+app.use('/style', express.static('./public/styles.css'))
 
-let traces=[]
+let students=[]
 
+// include and initialize the rollbar library with your access token
 var rollbar = new Rollbar({
-  accessToken: '48919cbac623421f8c4d079e8e09fe2e',
+  accessToken: '5520cd4cf12a4fe48fa8a32e60b9ee10',
   captureUncaught: true,
   captureUnhandledRejections: true
 });
 
+// record a generic message and send it to Rollbar
+
+
 app.get('/', (req, res)=>{
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-  rollbar.info('html file served successfully.')
-  })
-
-
-app.post('/api/trace', (req, res)=>{
- let {trace} = req.body
- 
-  const index = traces.findIndex(traceName=> traceName === trace)
-
-  if(index===-1 && trace !==""){
-      traces.push(" "+trace)
-      rollbar.log('trace added successfully', {author: 'Megan'})
-      res.status(200).send(traces)
-  } else if (trace===''){
-    rollbar.error('no trace given')
-    res.status(400).send('provide trace')
-  } else {
-    rollbar.error('already exists')
-    res.status(400).send('already exists')
-  }
+res.sendFile(path.join(__dirname, 'public/index.html'))
+rollbar.info('html file served successfully.')
 })
 
-const port=process.env.PORT || 5545
+app.post('/api/student', (req, res)=>{
+  let {name} = req.body
+  name = name.trim()
 
-app.listen(port, ()=>{console.log(`up and running on ${port}`)})
+  const index = students.findIndex(studentName=> studentName === name)
+
+  if(index === -1 && name !== ''){
+      students.push(name)
+      rollbar.log('student added successfully', {author: 'Scott'})
+      res.status(200).send(students)
+  } else if (name === ''){
+      rollbar.error('no name given', {author: 'megan'})
+      res.status(400).send('must provide a name.')
+  } else {
+      rollbar.error('student already exists', {author: 'megan'})
+      res.status(400).send('that student already exists')
+  }
+
+})
+
+const port=process.env.PORT || 4545
+
+rollbar.log("Hello world!");
+
+app.listen(port, ()=> console.log(`take us to a port of ${port}`))
